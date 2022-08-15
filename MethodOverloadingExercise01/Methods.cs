ed6a94a277f32fb44cb6ae8e1d062fac1b1a30d0
Cody <Cojo341@gmail.com>
2022-08-15 16:14:29 -0400

    Intial Commit

---
+++ MethodOverloadingExercise01/Methods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExercise01
+{
+    internal class Methods
+    {
+
+        public static int GetInput()
+        {
+            bool numCheck;
+
+            numCheck = int.TryParse(Console.ReadLine(), out int num);
+
+            while (!numCheck)
+            {
+                Console.WriteLine("Please enter a number: ");
+                numCheck = int.TryParse(Console.ReadLine(), out num);
+            }
+
+            return num;
+        }
+
+        public static double GetDoubleInput()
+        {
+            bool numCheck;
+
+            numCheck = double.TryParse(Console.ReadLine(), out double doubleNum);
+
+            while (!numCheck)
+            {
+                Console.WriteLine("Please enter a number: ");
+                numCheck = double.TryParse(Console.ReadLine(), out  doubleNum);
+            }
+
+            return doubleNum;
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static double Add(double firstNum, double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        public static string Add(int amountOne, int amountTwo, bool isTrue)
+        {
+            amountTwo *= 5;
+            var sum = amountOne + amountTwo;
+            return (isTrue) ? $"Your total is {sum} dollars" : $"Your total is {sum} dollar";
+        }
+    }
+}
---
+++ MethodOverloadingExercise01/Program.cs
@@ -0,0 +1,32 @@
+﻿using MethodOverloadingExercise01;
+
+
+
+Console.WriteLine("Hello, Enter the two numbers you'd like to add");
+var inputOne = Methods.GetInput();
+var inputTwo = Methods.GetInput();
+Console.WriteLine(Methods.Add(inputOne, inputTwo));
+
+
+Console.WriteLine("Now Enter two numbers with decimals you'd like to add");
+var decimalOne = Methods.GetDoubleInput();
+var decimalTwo = Methods.GetDoubleInput();
+Console.WriteLine(Methods.Add(decimalOne, decimalTwo));
+
+
+Console.WriteLine("Now lets count your money! \n"+"How Many One dollar bills do you have?");
+var dollarBills = Methods.GetInput();
+
+Console.WriteLine("Great! How many Five dollar bills do you have?");
+var fiveBills = Methods.GetInput();
+
+bool aboveOne;
+    if (fiveBills == 0 && dollarBills == 1)
+    {
+         aboveOne = false;
+    }
+    else
+    {
+         aboveOne=true;
+    }
+Console.WriteLine(Methods.Add(dollarBills, fiveBills, aboveOne));
